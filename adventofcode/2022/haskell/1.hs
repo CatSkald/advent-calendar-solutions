f60ea8ee79da926fa0999026b0481df44b30c1e3
CatSkald <CatSkald@users.noreply.github.com>
2022-12-08 22:25:57 +0100

    Cleanup code.

--- adventofcode/2022/haskell/1.hs
+++ adventofcode/2022/haskell/1.hs
@@ -6,10 +6,10 @@ import Data.List.Split
 makeInteger :: [String] -> [Int]
 makeInteger = map read
 
-getScore :: ([String], Int) -> Int
-getScore (fileLines, topX) = sum.take topX $ reverse.sort $ map (sum.makeInteger) (splitWhen (=="") fileLines)
+getScore :: [String] -> Int -> Int
+getScore fileLines topX = sum.take topX $ reverse.sort $ map (sum.makeInteger) (splitWhen (=="") fileLines)
 
 main = do
   fileLines <- fmap lines(readFile "../input/1input.txt")
-  print $ getScore (fileLines, 1)
-  print $ getScore (fileLines, 3)
+  print $ getScore fileLines 1
+  print $ getScore fileLines 3
--- adventofcode/2022/haskell/2.hs
+++ adventofcode/2022/haskell/2.hs
@@ -1,30 +1,30 @@
 module Main where
 
-getScore1 :: (Char, Char) -> Int
-getScore1 (x, y) = case (x, y) of
-  ('A', 'X') -> 3 + 1
-  ('B', 'Y') -> 3 + 2
-  ('C', 'Z') -> 3 + 3
-  ('C', 'X') -> 6 + 1
-  ('A', 'Y') -> 6 + 2
-  ('B', 'Z') -> 6 + 3
-  (_, 'X') -> 1
-  (_, 'Y') -> 2
-  (_, 'Z') -> 3
+getScore1 :: [Char] -> Int
+getScore1 x = case x of
+  ['A', _, 'X'] -> 3 + 1
+  ['B', _, 'Y'] -> 3 + 2
+  ['C', _, 'Z'] -> 3 + 3
+  ['C', _, 'X'] -> 6 + 1
+  ['A', _, 'Y'] -> 6 + 2
+  ['B', _, 'Z'] -> 6 + 3
+  [_, _, 'X'] -> 1
+  [_, _, 'Y'] -> 2
+  [_, _, 'Z'] -> 3
 
-getScore2 :: (Char, Char) -> Int
-getScore2 (x, y) = case (x, y) of
-  ('A', 'X') -> 3
-  ('B', 'X') -> 1
-  ('C', 'X') -> 2
-  ('A', 'Y') -> 3 + 1
-  ('B', 'Y') -> 3 + 2
-  ('C', 'Y') -> 3 + 3
-  ('A', 'Z') -> 6 + 2
-  ('B', 'Z') -> 6 + 3
-  ('C', 'Z') -> 6 + 1
+getScore2 :: [Char] -> Int
+getScore2 x = case x of
+  ['A', _, 'X'] -> 3
+  ['B', _, 'X'] -> 1
+  ['C', _, 'X'] -> 2
+  ['A', _, 'Y'] -> 3 + 1
+  ['B', _, 'Y'] -> 3 + 2
+  ['C', _, 'Y'] -> 3 + 3
+  ['A', _, 'Z'] -> 6 + 2
+  ['B', _, 'Z'] -> 6 + 3
+  ['C', _, 'Z'] -> 6 + 1
 
 main = do
   fileLines <- fmap lines(readFile "../input/2input.txt")
-  print $ sum $ map (getScore1.(\[x,y,z] -> (x,z))) fileLines
-  print $ sum $ map (getScore2.(\[x,y,z] -> (x,z))) fileLines
+  print $ sum $ map getScore1 fileLines
+  print $ sum $ map getScore2 fileLines
